0b62e16ac0276177ca91957e9594e91a1402cbc0
Marty Jones <mj8263788@gmail.com>
2021-10-13 04:13:38 -0400

    kernel: bump 5.14 to 5.14.12
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .155
-LINUX_VERSION-5.14 = .11
+LINUX_VERSION-5.14 = .12
 
 LINUX_KERNEL_HASH-5.4.155 = cc0c0f902748d251cb22d0e69e3b8d260c447eb0072ef853da15da6cce775038
-LINUX_KERNEL_HASH-5.14.11 = 93830a24df5342fb8fb857b7ca58d637f99d84b0ac54610fbf49743e3e8d5731
+LINUX_KERNEL_HASH-5.14.12 = 8d272679dc5285136bfe1f6d54548c7cf9f1ed8925c04b8bae149c8983eb5c37
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
