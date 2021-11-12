149c72dac0aeffadde7defd8bf79cfdcb0b2e8d5
Marty Jones <mj8263788@gmail.com>
2021-11-12 13:05:31 -0500

    kernel: bump 5.15 to 5.15.2
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .162
-LINUX_VERSION-5.15 = .1
+LINUX_VERSION-5.15 = .2
 
 LINUX_KERNEL_HASH-5.4.162 = c12d72ddaac78189305a5e98825295ecb02282970033b052276035e83189e25b
-LINUX_KERNEL_HASH-5.15.1 = 32fdcd33c8ac571b9a7a297f33860f6171327961f2a2ea6bd54bf82275b614c8
+LINUX_KERNEL_HASH-5.15.2 = 5634033a4981be42d3259f50d5371a2cdc9ace5d9860da67a2879630533ab175
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
