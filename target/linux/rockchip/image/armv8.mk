3134a013a584eb3340f5d0769e899fd1ac691e09
Marty Jones <mj8263788@gmail.com>
2023-05-06 18:26:01 -0400

    rockchip: add NanoPi R6S
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -20,3 +20,19 @@ define Device/friendlyelec_nanopi-r6c
 	collectd-mod-conntrack collectd-mod-irq dnsmasq-full -dnsmasq ethtool iperf3-ssl
 endef
 TARGET_DEVICES += friendlyelec_nanopi-r6c
+
+define Device/friendlyelec_nanopi-r6s
+  DEVICE_VENDOR := Friendlyelec
+  DEVICE_MODEL := NanoPi R6S
+  SOC := rk3588s
+  UBOOT_DEVICE_NAME := nanopi-r6s-rk3588
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r6c | pine64-img | gzip | append-metadata
+ DEVICE_PACKAGES := \
+	blkid block-mount blockd btrfs-progs f2fs-tools f2fsck fdisk fixparts fstools gdisk \
+	kmod-ata-ahci kmod-ata-core kmod-fs-autofs4 kmod-fs-btrfs kmod-fs-exfat kmod-fs-exportfs luci-app-ttyd \
+	kmod-fs-ext4 kmod-fs-squashfs kmod-nvme kmod-usb-net-rtl8152 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas \
+	kmod-usb-xhci-hcd kmod-usb3 losetup lsblk luci luci-proto-wireguard mkf2fs mount-utils parted partx-util pciutils resize2fs \
+	kmod-r8125 usbutils wget-ssl smartmontools luci-app-statistics collectd-mod-cpufreq collectd-mod-sensors collectd-mod-thermal \
+	collectd-mod-conntrack collectd-mod-irq dnsmasq-full -dnsmasq ethtool iperf3-ssl
+endef
+TARGET_DEVICES += friendlyelec_nanopi-r6s
