2b360b7ca8b749b924f20a19b881fffd77d290cd
Marty Jones <mj8263788@gmail.com>
2023-05-25 11:27:08 -0400

    rockchip: add FriendlyElec NanoPC-T6
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -5,6 +5,22 @@
 # FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
 KERNEL_LOADADDR := 0x03200000
 
+define Device/friendlyelec_nanopc-t6
+  DEVICE_VENDOR := Friendlyelec
+  DEVICE_MODEL := NanoPC T6
+  SOC := rk3588
+  UBOOT_DEVICE_NAME := nanopi-r6c-rk3588
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r6c | pine64-img | gzip | append-metadata
+ DEVICE_PACKAGES := \
+	blkid block-mount blockd btrfs-progs f2fs-tools f2fsck fdisk fixparts fstools gdisk \
+	kmod-ata-ahci kmod-ata-core kmod-fs-autofs4 kmod-fs-btrfs kmod-fs-exfat kmod-fs-exportfs luci-app-ttyd \
+	kmod-fs-ext4 kmod-fs-squashfs kmod-nvme kmod-usb-net-rtl8152 kmod-usb-storage kmod-usb-storage-extras kmod-usb-storage-uas \
+	kmod-usb-xhci-hcd kmod-usb3 losetup lsblk luci luci-proto-wireguard mkf2fs mount-utils parted partx-util pciutils resize2fs \
+	kmod-r8125 usbutils wget-ssl smartmontools luci-app-statistics collectd-mod-cpufreq collectd-mod-sensors collectd-mod-thermal \
+	collectd-mod-conntrack collectd-mod-irq dnsmasq-full -dnsmasq ethtool iperf3-ssl
+endef
+TARGET_DEVICES += friendlyelec_nanopc-t6
+
 define Device/friendlyelec_nanopi-r6c
   DEVICE_VENDOR := Friendlyelec
   DEVICE_MODEL := NanoPi R6C
