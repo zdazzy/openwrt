2dfe0bf99f785147b1f585626b91b0fca89aaedc
Marty Jones <mj8263788@gmail.com>
2023-09-04 20:12:24 -0400

    kernel: update to v6.5-rc7
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -11,33 +11,16 @@ define Device/firefly_roc-rk3328-cc
   SOC := rk3328
   DEVICE_DTS := rockchip/rk3328-roc-cc
   UBOOT_DEVICE_NAME := roc-cc-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += firefly_roc-rk3328-cc
-
-define Device/friendlyarm_nanopc-t4
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPC T4
-  SOC := rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-brcmfmac brcmfmac-nvram-4356-sdio cypress-firmware-4356-sdio
-endef
-TARGET_DEVICES += friendlyarm_nanopc-t4
-
-define Device/friendlyarm_nanopi-r2c
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R2C
-  SOC := rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r2c
 
 define Device/friendlyarm_nanopi-r2s
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R2S
   SOC := rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-usb-net-rtl8152
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
@@ -47,24 +30,17 @@ define Device/friendlyarm_nanopi-r4s
   DEVICE_MODEL := NanoPi R4S
   DEVICE_VARIANT := 4GB LPDDR4
   SOC := rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-r8169
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r4s
-
-define Device/pine64_rock64
-  DEVICE_VENDOR := Pine64
-  DEVICE_MODEL := Rock64
-  SOC := rk3328
-  UBOOT_DEVICE_NAME := rock64-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-endef
-TARGET_DEVICES += pine64_rock64
 
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
   SOC := rk3399
+  UBOOT_DEVICE_NAME := rockpro64-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += pine64_rockpro64
@@ -78,30 +54,3 @@ define Device/radxa_rock-pi-4a
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += radxa_rock-pi-4a
-
-define Device/radxa_rock-pi-e
-  DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := ROCK Pi E
-  SOC := rk3328
-  UBOOT_DEVICE_NAME := rock-pi-e-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-endef
-TARGET_DEVICES += radxa_rock-pi-e
-
-define Device/xunlong_orangepi-r1-plus
-  DEVICE_VENDOR := Xunlong
-  DEVICE_MODEL := Orange Pi R1 Plus
-  SOC := rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
-endef
-TARGET_DEVICES += xunlong_orangepi-r1-plus
-
-define Device/xunlong_orangepi-r1-plus-lts
-  DEVICE_VENDOR := Xunlong
-  DEVICE_MODEL := Orange Pi R1 Plus LTS
-  SOC := rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
-endef
-TARGET_DEVICES += xunlong_orangepi-r1-plus-lts
