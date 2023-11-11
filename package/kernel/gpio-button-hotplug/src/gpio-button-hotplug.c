b49a84d8af61a77dc9bfe55d92dc8c1983327d33
Marty Jones <mj8263788@gmail.com>
2023-11-11 00:04:01 -0500

    kernel: update to linux v6.7-rc0
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
+++ package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
@@ -538,8 +538,7 @@ static int gpio_keys_button_probe(struct platform_device *pdev,
 			struct device_node *child =
 				of_get_next_child(dev->of_node, prev);
 
-			bdata->gpiod = devm_gpiod_get_from_of_node(dev,
-				child, "gpios", 0, GPIOD_IN, desc);
+			bdata->gpiod = devm_gpiod_get_optional(dev, "reset", GPIOD_OUT_LOW);
 
 			prev = child;
 		}
