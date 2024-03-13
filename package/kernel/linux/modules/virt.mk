f9df63c93679cc329e1fa5585fa343b856ae4613
Marty Jones <mj8263788@gmail.com>
2024-03-13 02:38:28 -0400

    x86: add kernel 6.6
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- package/kernel/linux/modules/virt.mk
+++ package/kernel/linux/modules/virt.mk
@@ -84,9 +84,9 @@ define KernelPackage/vfio
 	CONFIG_VFIO_MDEV=n
   FILES:= \
 	$(LINUX_DIR)/drivers/vfio/vfio.ko \
-	$(LINUX_DIR)/drivers/vfio/vfio_virqfd.ko \
+	$(LINUX_DIR)/drivers/vfio/vfio_virqfd.ko@lt6.1 \
 	$(LINUX_DIR)/drivers/vfio/vfio_iommu_type1.ko
-  AUTOLOAD:=$(call AutoProbe,vfio vfio_iommu_type1 vfio_virqfd)
+  AUTOLOAD:=$(call AutoProbe,vfio vfio_iommu_type1 vfio_virqfd@lt6.1)
 endef
 
 define KernelPackage/vfio/description
