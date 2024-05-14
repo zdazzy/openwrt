0dda4ddb2b3757667f928fc6096740e28c306fc0
Marty Jones <mj8263788@gmail.com>
2024-05-13 20:34:35 -0400

    rockchip: add Linux 6.10
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -11,37 +11,16 @@ define Device/firefly_roc-rk3328-cc
   SOC := rk3328
   DEVICE_DTS := rockchip/rk3328-roc-cc
   UBOOT_DEVICE_NAME := roc-cc-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += firefly_roc-rk3328-cc
-
-define Device/friendlyarm_nanopc-t4
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPC T4
-  SOC := rk3399
-  DEVICE_PACKAGES := kmod-brcmfmac brcmfmac-nvram-4356-sdio cypress-firmware-4356-sdio
-endef
-TARGET_DEVICES += friendlyarm_nanopc-t4
-
-define Device/friendlyarm_nanopi-r2c
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R2C
-  SOC := rk3328
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r2c
-
-define Device/friendlyarm_nanopi-r2c-plus
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R2C Plus
-  SOC := rk3328
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r2c-plus
 
 define Device/friendlyarm_nanopi-r2s
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R2S
   SOC := rk3328
+  UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-usb-net-rtl8152
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
@@ -51,69 +30,20 @@ define Device/friendlyarm_nanopi-r4s
   DEVICE_MODEL := NanoPi R4S
   DEVICE_VARIANT := 4GB LPDDR4
   SOC := rk3399
+  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-r8169
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r4s
-
-define Device/friendlyarm_nanopi-r4s-enterprise
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R4S Enterprise Edition
-  DEVICE_VARIANT := 4GB LPDDR4
-  SOC := rk3399
-  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
-  DEVICE_PACKAGES := kmod-r8169
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r4s-enterprise
-
-define Device/friendlyarm_nanopi-r5c
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R5C
-  SOC := rk3568
-  DEVICE_PACKAGES := kmod-r8169 kmod-rtw88-8822ce rtl8822ce-firmware wpad-basic-mbedtls
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r5c
-
-define Device/friendlyarm_nanopi-r5s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R5S
-  SOC := rk3568
-  DEVICE_PACKAGES := kmod-r8169
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r5s
-
-define Device/pine64_rock64
-  DEVICE_VENDOR := Pine64
-  DEVICE_MODEL := Rock64
-  SOC := rk3328
-endef
-TARGET_DEVICES += pine64_rock64
 
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
   SOC := rk3399
+  UBOOT_DEVICE_NAME := rockpro64-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += pine64_rockpro64
-
-define Device/radxa_cm3-io
-  DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := CM3 IO
-  SOC := rk3566
-  DEVICE_DTS := rockchip/rk3566-radxa-cm3-io
-  UBOOT_DEVICE_NAME := radxa-cm3-io-rk3566
-endef
-TARGET_DEVICES += radxa_cm3-io
-
-define Device/radxa_e25
-  DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := E25
-  SOC := rk3568
-  DEVICE_DTS := rockchip/rk3568-radxa-e25
-  BOOT_SCRIPT := radxa-e25
-  UBOOT_DEVICE_NAME := radxa-e25-rk3568
-  DEVICE_PACKAGES := kmod-r8169 kmod-ata-ahci-platform
-endef
-TARGET_DEVICES += radxa_e25
 
 define Device/radxa_rock-pi-4a
   DEVICE_VENDOR := Radxa
@@ -121,37 +51,6 @@ define Device/radxa_rock-pi-4a
   SOC := rk3399
   SUPPORTED_DEVICES := radxa,rockpi4a radxa,rockpi4
   UBOOT_DEVICE_NAME := rock-pi-4-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += radxa_rock-pi-4a
-
-define Device/radxa_rock-pi-e
-  DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := ROCK Pi E
-  SOC := rk3328
-endef
-TARGET_DEVICES += radxa_rock-pi-e
-
-define Device/sinovoip_bpi-r2-pro
-  DEVICE_VENDOR := Sinovoip
-  DEVICE_MODEL := Bananapi-R2 Pro
-  SOC := rk3568
-  SUPPORTED_DEVICES := sinovoip,rk3568-bpi-r2pro
-  DEVICE_PACKAGES := kmod-ata-ahci-dwc
-endef
-TARGET_DEVICES += sinovoip_bpi-r2-pro
-
-define Device/xunlong_orangepi-r1-plus
-  DEVICE_VENDOR := Xunlong
-  DEVICE_MODEL := Orange Pi R1 Plus
-  SOC := rk3328
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
-endef
-TARGET_DEVICES += xunlong_orangepi-r1-plus
-
-define Device/xunlong_orangepi-r1-plus-lts
-  DEVICE_VENDOR := Xunlong
-  DEVICE_MODEL := Orange Pi R1 Plus LTS
-  SOC := rk3328
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
-endef
-TARGET_DEVICES += xunlong_orangepi-r1-plus-lts
