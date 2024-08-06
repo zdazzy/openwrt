d109a92862539b404a501319e27c34111a46fe70
Marty Jones <mj8263788@gmail.com>
2024-08-06 15:05:16 -0400

    rockchip: add wifi to radxa rock pi 4a v6.12
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -198,6 +198,7 @@ define Device/radxa_rock-pi-4a
   DEVICE_MODEL := ROCK Pi 4A
   SOC := rk3399
   SUPPORTED_DEVICES := radxa,rockpi4a radxa,rockpi4
+  DEVICE_PACKAGES := brcmfmac-firmware-43456-rockpi-4 kmod-brcmfmac wpad-basic-mbedtls
   UBOOT_DEVICE_NAME := rock-pi-4-rk3399
 endef
 TARGET_DEVICES += radxa_rock-pi-4a
