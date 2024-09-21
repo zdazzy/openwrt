3caae4b947b5293b81b0b6139f38bb65113d1ab6
Marty Jones <mj8263788@gmail.com>
2024-09-21 19:37:11 -0400

    kernel: Linux 6.11
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
+++ package/kernel/gpio-button-hotplug/src/gpio-button-hotplug.c
@@ -690,7 +690,6 @@ static int gpio_keys_remove(struct platform_device *pdev)
 
 static struct platform_driver gpio_keys_driver = {
 	.probe	= gpio_keys_probe,
-	.remove	= gpio_keys_remove,
 	.driver	= {
 		.name	= "gpio-keys",
 		.owner	= THIS_MODULE,
@@ -700,7 +699,6 @@ static struct platform_driver gpio_keys_driver = {
 
 static struct platform_driver gpio_keys_polled_driver = {
 	.probe	= gpio_keys_polled_probe,
-	.remove	= gpio_keys_remove,
 	.driver	= {
 		.name	= "gpio-keys-polled",
 		.owner	= THIS_MODULE,
